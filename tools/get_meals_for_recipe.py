4ae44bf712d96cdbc2b512b31cd405310555189c
PurpleDerg <sebastian.granda@upr.edu>
2026-01-09 19:16:58 -0400

    created tool that gets meal recipe

--- tools/get_meals_for_recipe.py
+++ tools/get_meals_for_recipe.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def search_meal_by_name(meal_name):
     """
@@ -14,7 +15,35 @@ def search_meal_by_name(meal_name):
     response.raise_for_status()  # raises error for bad HTTP responses
 
     data = response.json()
-    return data.get("meals")
+    meals = data.get("meals")
+    # meals = json.loads(meals)
+
+    # meals returns a list of jsons
+    counter = 0
+    ingredients = [] 
+    
+    for i in range(1, 20):
+        if meals[0][f"strIngredient{i}"] != "": 
+          ingredients.append(meals[0][f"strIngredient{i}"] + " " + meals[0][f"strMeasure{i}"])
+    
+    instruction = meals[0]["strInstructions"]
+    
+
+    filter_result = {
+       "instructions" : instruction, 
+       "ingredients" : ingredients, 
+    }
+    
+  
+
+    # filter_result = {key: meals[0][key] for key in ["strInstructions", "strIngredient1"]}
+
+
+    return filter_result
+
+
+    
+
 
 
 print(search_meal_by_name("Fettucine alfredo"))
